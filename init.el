86572206087df449783773360c5c82a49e464ec6
Jonathan Millett <jon@millett.net>
2022-11-27 12:25:26 -0300

    clojure lsp - disable breadcrumbs in header line

--- init.el
+++ init.el
@@ -519,11 +519,17 @@
 
 ;; --------------------------------------------------
 ;; LSP
+;; Ref: https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
+
 (use-package lsp-mode
   ;; :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  :init
+  (setq lsp-headerline-breadcrumb-enable nil)
+
   :custom
   (setq lsp-keymap-prefix "C-c l")
+
   :hook ((lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
